f5ff5a9329a9ec029bde8355653b9c6ad715d53f
labovaneng <47242079+labovaneng@users.noreply.github.com>
2019-02-01 11:26:10 +0100

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -199,3 +199,5 @@ Remove-Item "C:\Users\Public\Desktop\Bleach*"
 Install-WindowsUpdate -acceptEula -SuppressReboots -criteria "BrowseOnly=0 and IsAssigned=1 and IsHidden=0 and IsInstalled=0 and Type='Software'"
 
 Log-Message "Update Completed" | Out-File -Append "C:\Users\ENG\Desktop\Admin Tools\UpdateLog.txt"
+
+#aangepast Bart
